076e2cf14d14d19f38ba1d878611ba0241234fff
Imtiaz Anik <writetoimti@gmail.com>
2021-03-08 14:42:16 -0330

    test module added

---
+++ tests/tests.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+const { webscrapper } = require('../models/user.js');
+// const { webscrapper } = require('../models/product.js');
+const request = require('request');
+
+describe('Webscrapper Tests with Mocha', function(){
+    describe('Test Models', function(){
+        describe('Login', function(){
+   
+        });
+    });
+
+    
+});
