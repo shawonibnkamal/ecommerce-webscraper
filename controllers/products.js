1c9cf46feada3d6f7f84bb376586db153fb39f3a
shawonibnkamal <shawon.1165@gmail.com>
2021-03-07 22:36:50 -0330

    Product Controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,98 +1,281 @@
-const Product = require("../models/product.js");
+const express = require("express");
+const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
 
-const create = async (req, res) => {
-  const new_product = new Product(
-    parseInt(req.body.id),
-    req.body.name,
-    req.body.authors,
-    parseInt(req.body.year),
-    req.body.publisher
-  );
-  if (new_product.isValid()) {
-    let db = req.db;
-    try {
-      let msg = await new_product.save(db);
-      res.send(msg);
-    } catch (err) {
-      res.send(
-        "There was an error while saving your Product. (err:" + err + ")"
-      );
-      throw new Error(err);
+//requiring product model
+let Product = require("../models/product");
+
+//Scrape Function
+async function scrapeData(url, page) {
+  try {
+    await page.goto(url, { waitUntil: "load", timeout: 0 });
+    const html = await page.evaluate(() => document.body.innerHTML);
+    const $ = await cheerio.load(html);
+
+    let title = $("h1").attr("content");
+    let price = $(".price-characteristic").attr("content");
+
+    if (!price) {
+      let dollars = $(
+        "#price > div > span.hide-content.display-inline-block-m > span > span.price-group.price-out-of-stock > span.price-characteristic"
+      ).text();
+      let cents = $(
+        "#price > div > span.hide-content.display-inline-block-m > span > span.price-group.price-out-of-stock > span.price-mantissa"
+      ).text();
+      price = dollars + "." + cents;
     }
+
+    let seller = "";
+    let checkSeller = $(".seller-name");
+    if (checkSeller) {
+      seller = checkSeller.text();
+    }
+
+    let outOfStock = "";
+    let checkOutOfStock = $(".prod-ProductOffer-oosMsg");
+    if (checkOutOfStock) {
+      outOfStock = checkOutOfStock.text();
+    }
+
+    let deliveryNotAvaiable = "";
+    let checkDeliveryNotAvailable = $(".fulfillment-shipping-text");
+    if (checkDeliveryNotAvailable) {
+      deliveryNotAvaiable = checkDeliveryNotAvailable.text();
+    }
+
+    let stock = "";
+
+    if (
+      !seller.includes("Walmart") ||
+      outOfStock.includes("Out of Stock") ||
+      deliveryNotAvaiable.includes("Delivery not available")
+    ) {
+      stock = "Out of stock";
+    } else {
+      stock = "In stock";
+    }
+
+    return {
+      title,
+      price,
+      stock,
+      url,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const search = (req, res) => {
+  let userSku = req.query.sku;
+  if (userSku) {
+    Product.findOne({ sku: userSku })
+      .then((product) => {
+        if (!product) {
+          res.send("Product not found!");
+        }
+
+        res.send({ productData: product });
+      })
+      .catch((err) => {
+        res.send({ message: "There was an error" });
+      });
   } else {
-    res.send("client-side: The Product data you entered is invalid");
+    res.send({ productData: {} });
   }
 };
 
-const getOne = async (req, res) => {
-  const product_to_get = req.params.id;
-  let db = req.db;
+const getInStock = (req, res) => {
+  Product.find({ newstock: "In stock" })
+    .then((products) => {
+      res.send({ products: products });
+    })
+    .catch((err) => {
+      res.send({ message: "There was an error" });
+    });
+};
+
+const getOutOfStock = (req, res) => {
+  Product.find({ newstock: "Out of stock" })
+    .then((products) => {
+      res.send({ products: products });
+    })
+    .catch((err) => {
+      res.send({ message: "There was an error" + err });
+    });
+};
+
+const getPriceChanged = (req, res) => {
+  Product.find({})
+    .then((products) => {
+      res.send({ products: products });
+    })
+    .catch((err) => {
+      res.send({ message: "There was an error " + err });
+    });
+};
+
+const getBackInStock = (req, res) => {
+  Product.find({
+    $and: [{ oldstock: "Out of stock" }, { newstock: "In stock" }],
+  })
+    .then((products) => {
+      res.send({ products: products });
+    })
+    .catch((err) => {
+      res.send({ message: "There was an error " + err });
+    });
+};
+
+const getUpdated = (req, res) => {
+  Product.find({ updatestatus: "Updated" })
+    .then((products) => {
+      res.send({ products: products });
+    })
+    .catch((err) => {
+      res.send({ message: "There was an error " + err });
+    });
+};
+
+const getNotUpdated = (req, res) => {
+  Product.find({ updatestatus: "Not Updated" })
+    .then((products) => {
+      res.send({ products: products });
+    })
+    .catch((err) => {
+      res.send({ message: "There was an error " + err });
+    });
+};
+
+const fetchData = async (req, res) => {
   try {
-    let obj = await Product.getProductById(db, product_to_get);
-    res.send(obj);
-  } catch (err) {
-    res.send(
-      "There was an error while retrieving your Product. (err:" + err + ")"
-    );
-    throw new Error(err);
+    let url = req.query.search;
+    if (url) {
+      browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+      const page = await browser.newPage();
+      let result = await scrapeData(url, page);
+
+      let productData = {
+        title: result.title,
+        price: "$" + result.price,
+        stock: result.stock,
+        productUrl: result.url,
+      };
+      res.send({ productData: productData });
+      browser.close();
+    } else {
+      let productData = {
+        title: "",
+        price: "",
+        stock: "",
+        productUrl: "",
+      };
+      res.send({ productData: productData });
+    }
+  } catch (error) {
+    res.send({ message: "There was an error " + error });
   }
 };
 
-const updateOne = async (req, res) => {
-  const product_to_update = req.body;
-  const product_id = req.params.id;
-  let db = req.db;
+// Post request
+
+const postNewProduct = (req, res) => {
+  let { title, price, stock, url, sku } = req.body;
+
+  let newProduct = {
+    title: title,
+    newprice: price,
+    oldprice: price,
+    newstock: stock,
+    oldstock: stock,
+    sku: sku,
+    company: "Walmart",
+    url: url,
+    updatestatus: "Updated",
+  };
+
+  Product.findOne({ sku: sku })
+    .then((product) => {
+      if (product) {
+        res.send({ message: "Product already exist in the database." });
+      }
+
+      Product.create(newProduct).then((product) => {
+        res.send({ message: "Product added successfully in the database." });
+      });
+    })
+    .catch((err) => {
+      res.send({ message: "There was an error " + err });
+    });
+};
+
+const postUpdateProduct = async (req, res) => {
   try {
-    let msg = await Product.update(
-      db,
-      product_id,
-      product_to_update.name,
-      product_to_update.authors,
-      product_to_update.year,
-      product_to_update.publisher
-    );
-    res.send(msg);
-  } catch (err) {
-    res.send(
-      "There was an error while updating your Product. (err:" + err + ")"
-    );
-    throw new Error(err);
+    res.send({ message: "Products are updating." });
+
+    Product.find({})
+      .then(async (products) => {
+        for (let i = 0; i < products.length; i++) {
+          Product.updateOne(
+            { url: products[i].url },
+            {
+              $set: {
+                oldprice: products[i].newprice,
+                oldstock: products[i].newstock,
+                updatestatus: "Not Updated",
+              },
+            }
+          ).then((products) => {});
+        }
+
+        browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+        const page = await browser.newPage();
+
+        for (let i = 0; i < products.length; i++) {
+          let result = await scrapeData(products[i].url, page);
+          Product.updateOne(
+            { url: products[i].url },
+            {
+              $set: {
+                title: result.title,
+                newprice: "$" + result.price,
+                newstock: result.stock,
+                updatestatus: "Updated",
+              },
+            }
+          ).then((products) => {});
+        }
+      })
+      .catch((err) => {
+        res.send({ message: "There was an error " + err });
+      });
+  } catch (error) {
+    res.send({ message: "There was an error " + err });
   }
 };
 
-const deleteOne = async (req, res) => {
-  const product_id = req.params.id;
-  let db = req.db;
-  try {
-    let msg = await Product.delete(db, product_id);
-    res.send(msg);
-  } catch (err) {
-    res.send(
-      "There was an error while deleting your Product. (err:" + err + ")"
-    );
-    throw new Error(err);
-  }
+const deleteProduct = (req, res) => {
+  let searchQuery = { _id: req.params.id };
+
+  Product.deleteOne(searchQuery)
+    .then((product) => {
+      res.send({ message: "Product deleted successfully." });
+    })
+    .catch((err) => {
+      req.send({ message: "There was an error " + err });
+    });
 };
 
-const all = async (req, res) => {
-  let db = req.db;
-  try {
-    let obj = await Product.getProducts(db);
-    console.log("server-side: " + obj.length + " product(s) were returned");
-    res.send(obj);
-  } catch (err) {
-    res.send(
-      "There was an error while retrieving all Products. (err:" + err + ")"
-    );
-    throw new Error(err);
-  }
-};
-
-// Make all methods available for use.
 module.exports = {
-  create,
-  getOne,
-  updateOne,
-  deleteOne,
-  all,
+  search,
+  getInStock,
+  getOutOfStock,
+  getPriceChanged,
+  getBackInStock,
+  getUpdated,
+  getNotUpdated,
+  postNewProduct,
+  postUpdateProduct,
+  deleteProduct,
+  fetchData,
 };
--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,71 @@
 const express = require("express");
 const router = express.Router();
+const cheerio = require("cheerio");
 
-const product_controller = require("../controllers/products.js");
+const product_controller = require("../controllers/products");
 
-router.post("/", product_controller.create);
-router.get("/", product_controller.all);
-router.get("/:id", product_controller.getOne);
-router.put("/:id", product_controller.updateOne);
-router.delete("/:id", product_controller.deleteOne);
+//requiring product model
+const product = require("../models/product");
+
+// Checks if user is authenticated
+function isAuthenticatedUser(req, res, next) {
+  return next();
+  //   if (req.isAuthenticated()) {
+  //     return next();
+  //   }
+  //   req.flash("error_msg", "Please Login first to access this page.");
+  //   res.redirect("/login");
+}
+
+let browser;
+
+//GET routes starts here
+router.get("/search", isAuthenticatedUser, product_controller.search);
+
+router.get("/instock", isAuthenticatedUser, product_controller.getInStock);
+
+router.get(
+  "/outofstock",
+  isAuthenticatedUser,
+  product_controller.getOutOfStock
+);
+
+router.get(
+  "/pricechanged",
+  isAuthenticatedUser,
+  product_controller.getPriceChanged
+);
+
+router.get(
+  "/backinstock",
+  isAuthenticatedUser,
+  product_controller.getBackInStock
+);
+
+router.get("/updated", isAuthenticatedUser, product_controller.getUpdated);
+
+router.get(
+  "/notupdated",
+  isAuthenticatedUser,
+  product_controller.getNotUpdated
+);
+
+router.get("/fetch", isAuthenticatedUser, product_controller.fetchData);
+
+//POST routes starts here
+router.post("/new", isAuthenticatedUser, product_controller.postNewProduct);
+
+router.post(
+  "/update",
+  isAuthenticatedUser,
+  product_controller.postUpdateProduct
+);
+
+//DELETE routes starts here
+router.delete(
+  "/delete/:id",
+  isAuthenticatedUser,
+  product_controller.deleteProduct
+);
 
 module.exports = router;
