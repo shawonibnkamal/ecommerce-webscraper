bb5810806453a02828d013c7a7906387e2309435
shawonibnkamal <shawon.1165@gmail.com>
2021-04-02 12:41:43 -0230

    Move login, logout functions to users-ajax.js

--- public/scripts/users-ajax.js
+++ public/scripts/users-ajax.js
@@ -181,3 +181,107 @@ function deleteUserHandler(e, email) {
     },
   });
 }
+
+// Authentication ========================================================
+// =========================================================================
+
+// post request to login
+function login(e) {
+  e.preventDefault();
+
+  // get form data
+  let data = $(".login-box-body form").serialize();
+
+  // print data for debugging purpose
+  console.log(data);
+
+  // POST request
+  $.ajax({
+    type: "post",
+    url: "/users/login",
+    data: data,
+    dataType: "json",
+    success: function (response) {
+      // if succeeded
+      console.log(response);
+
+      // Store token in localstorage
+      localStorage.setItem("webscrapper-token", response.token);
+
+      // Display success message
+      $(".login-box-body .success").text(response.message);
+      $(".login-box-body .error").text("");
+
+      // Redirect to dashbaord page if sucessful
+      if (response.message != "Invalid credentials") {
+        location.href = "/dashboard";
+      } else {
+        // Else show error message to user
+        $(".login-box-body .success").text("");
+        $(".login-box-body .error").text("Invalid credentials");
+      }
+    },
+    error: function (error) {
+      // If any error occurs
+
+      // Print error message to console
+      console.error("An error has occured");
+      console.error(error.responseText);
+
+      // Show error message in dom
+      $(".login-box-body .error").text("An error has occured");
+      $(".login-box-body .success").text("");
+    },
+  });
+}
+
+// this function verifies if the user is logged in
+// by retrieving the token from local storage
+function getAuthenticatedUser() {
+  // get request
+  const token = localStorage.getItem("webscrapper-token");
+
+  // redirect to login page if token not present
+  if (!token) {
+    location.href = "/";
+  }
+
+  // verify token
+  // GET request
+  $.ajax({
+    type: "get",
+    url: "/users/profile?secret_token=" + token,
+    dataType: "json",
+    success: function (response) {
+      // Print response to console for debugging purpose
+      console.log(response);
+
+      // Set user_name in navbar
+      $("#user_name").text(response.user.name);
+      // Update url for edit user button
+      $("#user_edit").attr(
+        "href",
+        "/dashboard/users/edit?email=" + response.user.email
+      );
+    },
+    error: function (error) {
+      // Print error message in console
+      console.log("An error has occured");
+      console.error(error.responseText);
+
+      // redirect to login page if token not present
+      location.href = "/";
+    },
+  });
+}
+
+// logout user
+function logout(e) {
+  e.preventDefault();
+
+  // Remove token for localstorage
+  localStorage.removeItem("webscrapper-token");
+
+  // redirect to login page
+  location.href = "/";
+}
