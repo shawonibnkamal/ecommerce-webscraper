d5b6d7bac39a0af6abc998b0faf72a2cac94984a
shawonibnkamal <shawon.1165@gmail.com>
2021-04-02 09:42:15 -0230

    use port 3000

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const port = 80;
+const port = 3000;
 const path = require("path");
 const bodyParser = require("body-parser");
 const user_router = require("./routes/user");
