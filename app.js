397b9572c7a748d13d3d12e967adf84a7a78b5d2
shawonibnkamal <shawon.1165@gmail.com>
2021-04-02 09:38:17 -0230

    change port

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const port = 3000;
+const port = 80;
 const path = require("path");
 const bodyParser = require("body-parser");
 const user_router = require("./routes/user");
