a26f6cf3bffceba4bf8282af5c7fff3ab1607d88
shawonibnkamal <shawon.1165@gmail.com>
2021-04-10 17:23:48 -0230

    user.js Add Comments

--- routes/user.js
+++ routes/user.js
@@ -13,14 +13,23 @@ router.get(
   user_controller.getAuthenticatedUser
 );
 
+// Get a single user using unique email
 router.get("/:email", user_controller.getOne);
+
+// Put request to update a single user
 router.put("/:email", user_controller.updateOne);
+
+// Delete request to delete a user
 router.delete("/:email", user_controller.deleteOne);
+
+// Post request to register a new user
 router.post(
   "/signup",
   passport.authenticate("signup", { session: false }),
   user_controller.signup
 );
+
+// Post request to login using an existing user
 router.post("/login", user_controller.login);
 
 module.exports = router;
